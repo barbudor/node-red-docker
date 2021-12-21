42d4f6552c48850bdaf253cc53dd5fa07532722a
barbudor <barbudor@barbudor.net>
2021-12-21 20:46:35 +0100

    made i386 nodered:2.1.4-14

--- docker-custom/docker-alpine.sh
+++ docker-custom/docker-alpine.sh
@@ -6,11 +6,11 @@ echo "node-red version: ${NODE_RED_VERSION}"
 echo "#########################################################################"
 
 docker build --rm --no-cache \
-    --build-arg ARCH=amd64 \
+    --build-arg ARCH=i386 \
     --build-arg NODE_VERSION=14 \
     --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
     --build-arg OS=alpine3.12 \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
     --file Dockerfile.custom \
-    --tag testing:node-red-build .
+    --tag nodered/node-red:2.1.4-14 .
